5cb9dd5bc257750e3d45c961c2c70a87fad77f17
lucass <lucas.soares@trescon.com.br>
2023-04-25 19:29:48 -0300

    Primeiro commit

---
+++ src/styles/global.js
@@ -0,0 +1,44 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  input:focus { 
+    outline: none !important;
+  }
+
+  body {
+    background: ${(props) => props.theme.white};
+    color: ${(props) => props.theme.black};
+    -webkit-font-smoothing: antialiased;
+    overflow-x: hidden;
+  }
+
+  body, input, textarea, button {
+    font-family: ${({ theme }) => theme.fonts.regular}, sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  button {
+    cursor: pointer;
+  }
+    
+  ul {
+    list-style: none;
+  }
+
+  @media (max-width: 1366px) {
+    html {
+      font-size: 14px;
+    }
+  }
+`
---
+++ src/styles/themes/default.js
@@ -0,0 +1,20 @@
+import '../../../public/fonts/fonts.css'
+
+export const defaultTheme = {
+    colors: {
+        'base-white': '#FFFFFF',
+    },
+    textSizes: {
+        'title-l': '2.6rem',
+        'title-m': '2rem',
+        'title-ms': '1.6rem',
+        'title-s': '1.2rem',
+
+        'text-m': '1rem',
+        'text-s': '0.9rem',
+    },
+    fonts: {
+        regular: "'Poppins'",
+        title: "'Nordeco', sans-serif",
+    },
+}
